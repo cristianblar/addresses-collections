2552c3c6b3f5f850b235ea5a2200d813a3d48cfa
cristianblar <cristian@cristianblar.dev>
2022-01-12 19:15:24 -0500

    chore: fix the initial boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  globals: {
+    React: true,
+    JSX: true
+  },
+  rules: {
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
