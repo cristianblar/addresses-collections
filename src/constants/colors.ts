7ed2be49435c8ed4e30f40bcebc69c5a7d3ede8f
cristianblar <cristian@cristianblar.dev>
2022-01-12 21:03:39 -0500

    chore: create colors constant

---
+++ src/constants/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  light: 'rgba(255, 199, 159, 1)',
+  medium: 'rgba(255, 154, 116, 1)',
+  orange: 'rgba(233, 110, 76, 1)',
+  darkOrange: 'rgba(183, 66, 38, 1)',
+  red: 'rgba(134, 18, 0, 1)'
+}
