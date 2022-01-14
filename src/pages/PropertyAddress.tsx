8e49c4c53cd8c62f943c78f0fa8638671c5ceb0f
cristianblar <cristian@cristianblar.dev>
2022-01-14 16:38:13 -0500

    implement PropertyAddress page with base Form

--- src/pages/PropertyAddress.tsx
+++ src/pages/PropertyAddress.tsx
@@ -1,3 +1,35 @@
+import { Address } from 'models'
+import { Box, Paper, Typography } from '@mui/material'
+import { colors } from 'constant'
+import { Form } from 'components'
+import { useNavigate } from 'react-router-dom'
+
 export default function PropertyAddress(): JSX.Element {
-  return <div>Testing from PropertyAddress!</div>
+  const navigate = useNavigate()
+
+  const handleBack = () => navigate('/residential-address')
+
+  const handleSubmit = (values: Address) => {
+    console.log(values)
+    navigate('/employment-address')
+  }
+
+  return (
+    <Box marginX="auto" mt={6} textAlign="center" width="80%">
+      <Paper sx={{ backgroundColor: colors.light }} elevation={3}>
+        <Box p={2}>
+          <Box mb={4}>
+            <Typography variant="h5" color="primary" mb={2}>
+              Property address
+            </Typography>
+          </Box>
+          <Form
+            origin="property-address"
+            handleSubmit={handleSubmit}
+            handleBack={handleBack}
+          />
+        </Box>
+      </Paper>
+    </Box>
+  )
 }
