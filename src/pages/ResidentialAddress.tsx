a99e0ff5a1cf73b517e2221dc7e1b8f3c2e4b718
cristianblar <cristian@cristianblar.dev>
2022-01-14 16:37:56 -0500

    feat: implement ResidentialAddress page with base Form

--- src/pages/ResidentialAddress.tsx
+++ src/pages/ResidentialAddress.tsx
@@ -1,3 +1,35 @@
+import { Address } from 'models'
+import { Box, Paper, Typography } from '@mui/material'
+import { colors } from 'constant'
+import { Form } from 'components'
+import { useNavigate } from 'react-router-dom'
+
 export default function ResidentialAddress(): JSX.Element {
-  return <div>Testing from ResidentialAddress!</div>
+  const navigate = useNavigate()
+
+  const handleBack = () => navigate('/')
+
+  const handleSubmit = (values: Address) => {
+    console.log(values)
+    navigate('/property-address')
+  }
+
+  return (
+    <Box marginX="auto" mt={6} textAlign="center" width="80%">
+      <Paper sx={{ backgroundColor: colors.light }} elevation={3}>
+        <Box p={2}>
+          <Box mb={4}>
+            <Typography variant="h5" color="primary" mb={2}>
+              Residential address
+            </Typography>
+          </Box>
+          <Form
+            origin="residential-address"
+            handleSubmit={handleSubmit}
+            handleBack={handleBack}
+          />
+        </Box>
+      </Paper>
+    </Box>
+  )
 }
