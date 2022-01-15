a8abcdd9602a57f04c5db1fd77d7a993345576a6
cristianblar <cristian@cristianblar.dev>
2022-01-14 20:51:19 -0500

    feat: filter routes based on requirement

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,25 @@
 import { routes } from './routes'
 import { Routes, Route } from 'react-router-dom'
+import { useAppSelector, selectCustomerAddresses } from 'store'
 import Home from 'pages/Home'
+import { NotFound } from 'components'
 
 export function Router(): JSX.Element {
+  const customerData = useAppSelector(selectCustomerAddresses)
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {routes.map(({ Component, path }, idx) => (
-        <Route key={idx + path} element={<Component />} path={path} />
-      ))}
+      {routes
+        .filter(({ require }) =>
+          !require
+            ? true
+            : Object.entries(customerData[require]).every((tuple) => !!tuple[1])
+        )
+        .map(({ Component, path }, idx) => (
+          <Route key={idx + path} element={<Component />} path={path} />
+        ))}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
